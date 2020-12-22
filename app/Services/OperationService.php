08a803d748ec3a07a4882f64f9b9bdd7c1fee9d4
natf3 <n.frankiewicz@gmail.com>
2020-12-22 04:15:40 +0100

    create service OperationForClient

---
+++ app/Services/OperationService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services;
+
+use App\Exceptions\BankNotInitializedException;
+use App\Models\Transaction;
+use App\Models\Operation;
+use App\Models\BankingAccount;
+use App\Models\User;
+use Exception;
+
+class UserService
+{
+    public function createOperationForClient(Transaction $transaction)
+    {
+        if($transaction->prin_banking_account_id != "null"){
+        $operation = Operation::create([
+
+            "transaction_id" => $transaction->id,
+            "nrb_ben" => $transaction->nrb_ben,
+            "name_ben" => $transaction->name_ben,
+            "address_ben" => $transaction->address_ben,
+            "amount" => $transaction->amount,
+            "posting_date" => "null",
+            "nrb_prin" => $transaction->nrb_prin,
+            "address_prin" => $transaction->address_prin,
+            "name_prin" => $transaction->name_prin,
+            "prin_banking_account_id" => $transaction->prin_banking_account_id,
+            "ben_banking_account_id" => $transaction->ben_banking_account_id
+
+            ]);
+        $moneySent = $transaction->amount;
+        $prinBankingAccount = BankingAccount::find($transaction->prin_banking_account_id);
+        $prinBankingAccount ->balance -= $moneySent;
+        
+        $prinBankingAccount ->save();
+
+        $benBankingAccount = BankingAccount::find($transaction->ben_banking_account_id);
+        $benBankingAccount ->balance += $moneySent;
+
+        $benBankingAccount->save();
+
+        return $operation;
+        }
+        else {
+            $operation = Operation::create([
+
+                "transaction_id" => $transaction->id,
+                "nrb_ben" => $transaction->nrb_ben,
+                "name_ben" => $transaction->name_ben,
+                "address_ben" => $transaction->address_ben,
+                "amount" => $transaction->amount,
+                "posting_date" => "null",
+                "nrb_prin" => $transaction->nrb_prin,
+                "address_prin" => $transaction->address_prin,
+                "name_prin" => $transaction->name_prin,                
+                "prin_banking_account_id" => $transaction->prin_banking_account_id               
+                ]);
+                
+        $moneySent = $transaction->amount;
+        $prinBankingAccount = BankingAccount::find($transaction->prin_banking_account_id);
+        $prinBankingAccount ->balance -= $moneySent;
+        
+        $prinBankingAccount ->save();
+
+        return $operation;
+        }   
+
+
+     }
+     
+}
